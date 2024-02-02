16e77a649436de86cada2a38ba26777f2383ec22
Ian McNabb <ianmcnabb08@gmail.com>
2024-02-01 21:06:57 -0600

    Not sure what happened. Pushing again.

---
+++ client/src/views/Main.js
@@ -0,0 +1,40 @@
+// Main.js
+
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
+
+function Main() {
+    const [city, setCity] = useState('');
+
+    const fetchWeather = async () => {
+        try {
+        // Fetch weather data
+        const { data: weatherResponse } = await axios.get(`/api/weather/${city}`);
+        } catch (error) {
+        console.error(error);
+        }
+    };
+
+    return (
+        <div className="main-container">
+        <div className="city-selection" id='form'>
+            <h1>MyWeather<span id='label-span'>App</span></h1>
+            <input
+            type="text"
+            placeholder="Enter city"
+            value={city}
+            onChange={(e) => setCity(e.target.value)}
+            id="city"
+            />
+            <Link to={`/details/${city}`}>
+            <button onClick={fetchWeather} id="button">
+                Get Weather
+            </button>
+            </Link>
+        </div>
+        </div>
+    );
+}
+
+export default Main;
